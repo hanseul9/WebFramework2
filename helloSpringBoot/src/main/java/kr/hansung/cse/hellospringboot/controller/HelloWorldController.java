c0ece3ea262901affbc06fd2130c8ec806456ef9
hanseul9 <hs990130@naver.com>
2023-06-03 17:12:06 +0900

    thymeleaf, application property binding

--- helloSpringBoot/src/main/java/kr/hansung/cse/hellospringboot/controller/HelloWorldController.java
+++ helloSpringBoot/src/main/java/kr/hansung/cse/hellospringboot/controller/HelloWorldController.java
@@ -1,13 +1,39 @@
 package kr.hansung.cse.hellospringboot.controller;
 
+import kr.hansung.cse.hellospringboot.conf.AppConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloWorldController {
 
+//    @Value("${app.professor}")
+//    private String professor;
+//    @Value("${app.course}")
+//    private String course;
+
+    @Autowired
+    private AppConfig appConfig;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("/")
-    public String sayHello() {
+    public String sayHello(Model model) {
+
+//        logger.debug("professor = " + professor);
+//        logger.debug("course = " + course);
+
+        logger.debug("professor = " + appConfig.getProfessor());
+        logger.debug("course = " + appConfig.getCourse());
+
+
+        model.addAttribute("msg", "hello world");
+        model.addAttribute("appConfig", appConfig);
+
         return "index";
     }
 
